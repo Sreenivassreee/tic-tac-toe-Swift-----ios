852981537e4597555cf4e3b5822090593e278760
Sreenivassreee <ksreenivascse@gmail.com>
2021-05-09 22:39:33 +0530

    reset remains

--- tic tac toe/ViewController.swift
+++ tic tac toe/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 class ViewController: UIViewController {
     var isGameOver = false
     var player=1
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        resetButton.isHidden = true
+        self.One.setTitle("X", for: .normal)
         // Do any additional setup after loading the view.
     }
-
+    @IBOutlet weak var WinnerLable: UILabel!
+    
     @IBOutlet weak var One: UIButton!
     @IBOutlet weak var Two: UIButton!
     @IBOutlet weak var Three: UIButton!
@@ -26,11 +29,106 @@ class ViewController: UIViewController {
     @IBOutlet weak var Eight: UIButton!
     @IBOutlet weak var Nine: UIButton!
     
+    @IBOutlet weak var resetButton: UIButton!
+  
+    @IBAction func resetPressed(_ sender: UIButton) {
+        
+        
+        refreshView()
+      
+        
+    }
+    
+   
+    
+    
     @IBAction func One(_ sender: UIButton) {
-//        placeingObject(with: sender)
-//        checkWinner()
+        print("Here")
+        placeingObject(with: sender)
+        checkWinner()
     }
     
     
+    
+    func placeingObject(with sender:UIButton){
+        if isGameOver{
+            refreshView()
+        }else{
+            print(sender.titleLabel?.text)
+            if sender.titleLabel?.text == nil {
+                if self.player==1{
+                    sender.setTitleColor(.white, for: .normal)
+                    sender.setTitle("X", for: .normal)
+                    player = 2
+                }else{
+                    sender.setTitleColor(.white, for: .normal)
+                    sender.setTitle("O", for: .normal)
+                    player = 1
+                }
+            }else{
+                print("already Exist")
+            }
+        }
+        
+    }
+    
+    func refreshView(){
+
+        DispatchQueue.main.async {
+            self.isGameOver = false
+            self.One.setTitle(nil, for: .normal)
+            self.Two.setTitle(nil, for: .normal)
+            self.Three.setTitle("", for: .normal)
+            self.Four.setTitle(nil, for: .normal)
+            self.Five.setTitle(nil, for: .normal)
+            self.Six.setTitle(nil, for: .normal)
+            self.Seven.setTitle(nil, for: .normal)
+            self.Eight.setTitle(nil, for: .normal)
+            self.Nine.setTitle(nil, for: .normal)
+            
+            self.WinnerLable.text=""
+            self.player=1
+            self.resetButton.isHidden=true
+        }
+
+    }
+
+    func checkWinner() {
+        var one = One.titleLabel?.text
+        let two = Two.titleLabel?.text
+        let three = Three.titleLabel?.text
+        let four = Four .titleLabel?.text
+        let five = Five.titleLabel?.text
+        let six = Six.titleLabel?.text
+        let seven = Seven.titleLabel?.text
+        let eight = Eight.titleLabel?.text
+        let nine = Nine.titleLabel?.text
+        if !isGameOver{            
+            if (one == "X" && two == "X" && three == "X") || (four == "X" && five == "X" && six == "X") || (seven == "X" && eight == "X" && nine == "X") || (one == "X" && four == "X" && seven == "X") || (two == "X" && five == "X" && eight == "X") || (three == "X" && six == "X" && nine == "X"){
+                WinnerLable.text="Winner : X"
+                resetButton.isHidden=false
+                isGameOver=true
+            }else if(one == "X" && five == "X" && nine == "X") || (three == "X" && five == "X" && seven == "X"){
+                WinnerLable.text="Winner : X"
+                resetButton.isHidden=false
+                
+                isGameOver=true
+            }else if (one == "O" && two == "O" && three == "O") || (four == "O" && five == "O" && six == "O") || (seven == "O" && eight == "O" && nine == "O") || (one == "O" && four == "O" && seven == "O") || (two == "O" && five == "O" && eight == "O") || (three == "O" && six == "O" && nine == "O"){
+                WinnerLable.text="Winner : O"
+                resetButton.isHidden=false
+                isGameOver=true
+            }else if(one == "O" && five == "O" && nine == "O") || (three == "O" && five == "O" && seven == "O"){
+                WinnerLable.text="Winner : O"
+                resetButton.isHidden=false
+                isGameOver=true
+            }else if one != nil && two != nil && three != nil && four != nil && five != nil && six != nil && seven != nil && eight != nil && nine != nil{
+                WinnerLable.text="Match Draw"
+                resetButton.isHidden=false
+                }
+                
+            
+        }
+        
+    }
 }
 
