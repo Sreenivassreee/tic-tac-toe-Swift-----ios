21589d33cb08c1167e3f91a0b11662c92ab1cf6e
Sreenivassreee <ksreenivascse@gmail.com>
2021-05-09 20:30:19 +0530

    UI Completed

--- tic tac toe/ViewController.swift
+++ tic tac toe/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isGameOver = false
+    var player=1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var One: UIButton!
+    @IBOutlet weak var Two: UIButton!
+    @IBOutlet weak var Three: UIButton!
+    @IBOutlet weak var Four: UIButton!
+    @IBOutlet weak var Five: UIButton!
+    @IBOutlet weak var Six: UIButton!
+    @IBOutlet weak var Seven: UIButton!
+    @IBOutlet weak var Eight: UIButton!
+    @IBOutlet weak var Nine: UIButton!
+    
+    @IBAction func One(_ sender: UIButton) {
+//        placeingObject(with: sender)
+//        checkWinner()
+    }
+    
+    
 }
 
